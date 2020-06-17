2eca858b60c3f1bddb08082112050737989cfce6
yiguo-17 <yiguo.qu@codeimmersives.com>
2020-06-17 07:17:12 -0400

    revised based on test

--- color-combinator.js
+++ color-combinator.js
@@ -1,11 +1,11 @@
-const pmrcl = ['red', 'yellow', 'blue'];
 
 function newcolor(cl1, cl2){
-  let combo = [cl1.indexOf(pmrcl), cl2.indexOf(pmrcl)].sort();
-  if(combo === [0,1]){return 'When you combine '+ cl1 +' and '+cl2+', you get orange!'}
-  if(combo === [0,2]){return 'When you combine '+ cl1 +' and '+cl2+ ', you get purple'}
-  if(combo === [1,2]{return 'When you combine '+ cl1 +' and '+cl2+', you get green!'}
-  if(combo[0] === -1){return 'Sorry, one of those colors is not a primary color!';}
+  const pmrcl = ['red', 'yellow', 'blue'];
+  const combo = String([pmrcl.indexOf(cl1), pmrcl.indexOf(cl2)].sort());
+  if(combo === '0,1'){return 'When you combine '+ cl1 +' and '+cl2+', you get orange!'}
+  if(combo === '0,2'){return 'When you combine '+ cl1 +' and '+cl2+ ', you get purple!'}
+  if(combo === '1,2'){return 'When you combine '+ cl1 +' and '+cl2+', you get green!'}
+  if(combo[0] === '-'){return 'Sorry, one of those colors is not a primary color!';}
   
 }
-module.exports = colorCombinator;
+module.exports = newcolor;
--- color-deconstructor.js
+++ color-deconstructor.js
@@ -1,8 +1,9 @@
-const scdcl = ['orange', 'purple', 'green'];
+
 function comefrom(cl){
+  const scdcl = ['orange', 'purple', 'green'];
   if(cl === scdcl[0]){return 'Orange is made by combining red and yellow!';}
   if(cl === scdcl[1]){return 'Purple is made by combining red and blue!';}
   if(cl === scdcl[2]){return 'Green is made by combining blue and yellow!';}
-  if(cl.indexOf(scdcl) === -1){return "Sorry, that's not a secondary color!";}
+  if(scdcl.indexOf(cl) === -1){return "Sorry, that's not a secondary color!";}
 }
-module.exports = colorDeconstructor;
+module.exports = comefrom;
